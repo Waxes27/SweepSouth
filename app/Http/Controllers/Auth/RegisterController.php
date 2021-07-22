6ba3df16c7f1f90ae5ab1b691fe209600535419c
waxes27 <NCEBA.DUMASI@GMAIL.COM>
2021-07-22 02:22:52 +0200

    registration done'

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-// use App\Models\Employee;
 use App\Models\User;
 
 class RegisterController extends Controller
