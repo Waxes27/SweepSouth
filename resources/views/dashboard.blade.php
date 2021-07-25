b981db0dc32f654f950d64fce39e4210e8d0ffe1
waxes27 <NCEBA.DUMASI@GMAIL.COM>
2021-07-25 16:15:07 +0200

    Services page addition

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,17 +7,31 @@
 @section('content')
 <body>
     {{-- for partners --}}
-    <div class="flex flex-wrap p-3 bg-red-300">
+    <div class="flex flex-wrap p-3 ">
 
         <h1 class="text-3xl w-full text-center mb-3">
             Your services
         </h1>
-        <div class="flex flex-wrap justify-between">
+        <div class="grid grid-cols-3 gap-4 w-full flex-wrap">
             @if ($services->count())
                 @foreach ($services as $service )
-                    <div class="bg-blue-500">
-                        {{$service}}
-                        {{ auth()->user()->id }}
+                    <div class=" border p-3 rounded">
+                        <div class="text-center w-full">
+                            <h1 class=" bg-blue-300 rounded">Type</h1>
+                            <div class="mb-6 bg-white rounded">
+                                {{ $service->type }}
+                            </div>
+
+                            <h1 class=" bg-blue-300 rounded">description</h1>
+                            <div class="mb-6 bg-white rounded">
+                                {{$service->description}}
+                            </div>
+
+                            <h1 class="bg-blue-300 rounded">Price</h1>
+                            <div class="mb-6 bg-white rounded">
+                                R{{$service->price}}
+                            </div>
+                        </div>
                     </div>
                 @endforeach
             @endif
@@ -25,8 +39,8 @@
 
 
     </div>
-    <a href=" {{ route('create_service') }} ">
-        <button class="mb-6 bg-blue-400 rounded hover:text-white p-3 w-full">
+    <a href="{{ route('create_service') }}">
+        <button class="mt-6 bg-blue-400 rounded hover:text-white p-3 w-full">
             Create New Service
         </button>
     </a>
